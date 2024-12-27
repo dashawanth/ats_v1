10505b9c74820dbe799a63c5ce0b6f57ba741c09
Dashawanth Reddy <145147481+dashawanth@users.noreply.github.com>
2024-12-27 16:00:44 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def load_data():
-    recruiter_detail = pd.read_csv('data/recruiter_detail.csv')
-    job_requirements = pd.read_csv('data/job_requirements.csv')
-    submission_table = pd.read_csv('data/submission_table.csv')
+    recruiter_detail = pd.read_csv('s3://ats-files1/data/recruiter_detail.csv')
+    job_requirements = pd.read_csv('s3://ats-files1/data/job_requirements.csv')
+    submission_table = pd.read_csv('s3://ats-files1/data/submission_table.csv')
     return recruiter_detail, job_requirements, submission_table
